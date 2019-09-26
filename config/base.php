4f23a230ef3d496cc893d727b957205c20c1d2f6
OTeeEnabor <tobienabor@gmail.com>
2019-09-26 13:07:51 +0200

    connection variable details.

---
+++ config/base.php
@@ -0,0 +1,12 @@
+<?php 
+//connect to the hotelbooking  database
+//name of the server 
+define('DB_SERVER','localhost');
+//name of the user
+define('DB_USER','emailadmin');
+//user password
+define('DB_PASSWORD','mailform');
+//name database
+define('DB_NAME','contactusexample');
+
+?>
